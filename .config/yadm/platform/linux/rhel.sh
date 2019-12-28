4aca0eb4adabbdc9e00af59c46f762686c5a30a3
Marc Patton <marc.patton@proquest.com>
2019-12-28 03:30:01 -0500

    Fix issues with CentOS setup
    1. `fzf` installation was in wrong directory
    2. The variable `$MYADM_COMMON_PKGS` can't be unset, so use `/bin/true`
    as the `$MYADM_CHECK_INSTALLED_CMD` for CentOS

--- .config/yadm/platform/linux/rhel.sh
+++ .config/yadm/platform/linux/rhel.sh
@@ -1,11 +1,17 @@
 # Remember, we are assuming the inability/aversion to install additional  
 # packages, so we don't want much to happen here
-MYADM_CHECK_INSTALLED_CMD=(yum list installed)
+# The check installed command may be useful in the future, so I'm leaving  
+# it here for now
+# MYADM_CHECK_INSTALLED_CMD=(yum list installed)
+
+# This workaround is here because I can't unset the common pkgs varaiable; 
+# it's read only.  I could change that, but I don't want to.
+MYADM_CHECK_INSTALLED_CMD=(/bin/true)
 MYADM_INSTALL_CMD=(sudo yum install)
 MYADM_OS_PKGS=()
 
 _USER_BIN_PATH="${HOME}"/.local/bin
-_FZF_INSTALL_PATH="${_USER_BIN_PATH}"/fzf
+_FZF_INSTALL_PATH="${HOME}"/.local/fzf
 _FZF_URL="https://github.com/junegunn/fzf.git"
 _RG_VERSION=11.0.2
 _RG_TARBALL="ripgrep-${_RG_VERSION}-x86_64-unknown-linux-musl.tar.gz"
@@ -23,7 +29,7 @@ _install_fzf() {
 _install_ripgrep() {
   curl -L -O "${_RG_URL}"
   tar xzf "${_RG_TARBALL}"
-  cp "${_RG_INSTALL_PATH}"/rg "${_USER_BIN_PATH}"/rg
+  cp "${_RG_INSTALL_PATH}"/rg "${_USER_BIN_PATH}"
   rm "${_RG_TARBALL}"
   rm -rf "${_RG_INSTALL_PATH}"
 }
@@ -32,7 +38,6 @@ _install_ripgrep() {
 myadm_pkg_mgr_prep() {
   yellow_echo "WARNING: No packages will be installed system-wide."
   yellow_echo "Installing fzf and ripgrep for current user."
-  unset MYADM_COMMON_PKGS
   unset MYADM_OS_VIM_PLUGINS
 
   [[ ! -d "${_USER_BIN_PATH}" ]] && mkdir -p "${_USER_BIN_PATH}"
