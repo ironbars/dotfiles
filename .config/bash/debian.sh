bf6ba0c787625d510b83381ecf2db7c7b95d34ba
Marc Patton <iron.bars85@gmail.com>
2020-06-13 12:10:32 -0400

    Fix typo

--- .config/bash/debian.sh
+++ .config/bash/debian.sh
@@ -3,6 +3,7 @@ pkg_remove="sudo apt-get autoremove"
 pkg_search="apt-cache search"
 
 # This is necessary due to some weirdness with WSL 1 and Rust.  
-if [[ $(uname -r) = Microsoft* ]]; then
+if [[ $(uname -r) = *Microsoft ]]; then
   fzf_rg_opts+=(--threads 1)
+  alias rg='rg --threads 1'
 fi
