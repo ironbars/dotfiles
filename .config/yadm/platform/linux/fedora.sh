07a18f709f0de5805ce79a1bf5321146f4f36e7c
Marc Patton <iron.bars85@gmail.com>
2020-10-31 20:28:35 -0600

    Update fedora configs

--- .config/yadm/platform/linux/fedora.sh
+++ .config/yadm/platform/linux/fedora.sh
@@ -4,8 +4,5 @@ MYADM_OS_PKGS=("ncompress" "p7zip-plugins" "bzip2")
 
 
 myadm_pkg_mgr_prep() {
-  sudo dnf install -y \
-    https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-30.noarch.rpm
-  sudo dnf install -y \
-    https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-30.noarch.rpm
+  return
 } 
